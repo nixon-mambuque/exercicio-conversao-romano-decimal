a4eaaf71f2c5dd112cb358906f70afa20b15b978
Nixon <nixon@gmail.com>
2016-08-26 13:03:25 +0200

    Implementando a lógica para a conversão

--- src/mz/nixon/mambuque/conversor/ConversorDeRomanoParaDecimal.java
+++ src/mz/nixon/mambuque/conversor/ConversorDeRomanoParaDecimal.java
@@ -4,11 +4,62 @@ public class ConversorDeRomanoParaDecimal {
 	
 	public int converte(String romano){
 		
+		int valorActual = 0;
+		int ultimoValor = 0;
 		if(romano.length()==1){
 			return (int) (Math.floor(Math.pow(10, "IXCM".indexOf(romano.charAt(0))))+
 					5*Math.floor(Math.pow(10, "VLD".indexOf(romano.charAt(0)))));
 		}
 		
+		else{
+			
+			for(int i = romano.length()-1; i >= 0 ; i--){
+				switch (romano.charAt(i)) {
+				case 'M':
+					valorActual = logicaDeConversao(1000, valorActual, ultimoValor);
+					ultimoValor = 1000;
+					break;
+					
+				case 'D':
+					valorActual = logicaDeConversao(500, valorActual, ultimoValor);
+					ultimoValor = 500;
+					break;
+					
+				case 'C':
+					valorActual = logicaDeConversao(100, valorActual, ultimoValor);
+					ultimoValor = 100;
+					break;
+					
+				case 'L':
+					valorActual = logicaDeConversao(50, valorActual, ultimoValor);
+					ultimoValor = 50;
+					break;
+					
+				case 'X':
+					valorActual = logicaDeConversao(10, valorActual, ultimoValor);
+					ultimoValor = 10;
+					break;
+					
+				case 'V':
+					valorActual = logicaDeConversao(5, valorActual, ultimoValor);
+					ultimoValor = 5;
+					break;
+					
+				case 'I':
+					valorActual = logicaDeConversao(1, valorActual, ultimoValor);
+					ultimoValor = 1;
+					break;
+
+				default:
+					break;
+				}
+			}
+		}
+		
+		return 0;
+	}
+	
+	public int logicaDeConversao(int valalorActual, int valorAnterior, int ultimoValor){
 		return 0;
 	}
 }
